70530882a6f8d7c06488f5779d96303c010078eb
David Mendes <davidgmendes@hotmail.com>
2021-05-13 03:40:54 -0300

    View beeem básica feita, se eu tiver tempo nos próximos dias faço algo mais complexo

--- db_scripts/procedure.sql
+++ db_scripts/procedure.sql
@@ -66,3 +66,18 @@ END
 //
 
 call Batalha(1, "Jinx", "Goblin");
+
+DROP view if exists Personagens_Sven;
+create view Personagens_Sven as select 
+	Personagem.nome as Personagem, Classe_FK, Pontos_Vida, Pontos_Mana, Nível, Arma_FK,
+    Armadura_FK, Defesa, Dano, Missão.Nome as Missão, Dungeon.Nome as Dungeon
+    FROM Atributo
+    RIGHT JOIN Personagem on Atributo_FK = IdAtributo
+    LEFT JOIN Missões_Personagem on Personagem_FK = Personagem.Nome
+    LEFT JOIN Missão on Missões_Personagem.Missão_FK = Missão.Nome
+    LEFT JOIN Missões_Dungeon on Missões_Dungeon.Missão_FK = Missão.Nome
+    LEFT JOIN Dungeon on Dungeon_FK = Dungeon.Nome
+    WHERE NPC_FK = "Sven";
+    
+select * from Missões_Sven;
+	
